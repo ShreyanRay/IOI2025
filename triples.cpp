fd00cb4f724bd6a24c0668c405d32109c65b2334
ShreyanRay <109859584+ShreyanRay@users.noreply.github.com>
2025-08-01 00:40:11 +0530

    Update triples.cpp

--- triples.cpp
+++ triples.cpp
@@ -1,3 +1,4 @@
+#include "triples.h"
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
@@ -5,7 +6,8 @@ using namespace std;
 
 mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
 
-int smart(int n, vector <int> a){
+int count_triples(vector <int32_t> a){
+    int n = a.size();
     int ans = 0;
     for (int i = 0; i < n; i++){
         if (i + a[i] < n){
@@ -97,51 +99,6 @@ int smart(int n, vector <int> a){
     return ans;
 }
 
-int brute(int n, vector <int> a){
-    int ans = 0;
-    for (int i = 0; i < n; i++){
-        for (int j = i + 1; j < n; j++){
-            for (int k = j + 1; k < n; k++){
-                vector <int> v1 = {a[i], a[j], a[k]};
-                vector <int> v2 = {j - i, k - j, k - i};
-                sort(v1.begin(), v1.end());
-                sort(v2.begin(), v2.end());
-                
-                ans += (v1 == v2);
-            }
-        }
-    }
-    
-    return ans;
-}
-
-void Solve() 
-{
-    int n; cin >> n;
-    
-    vector <int> a(n);
-    for (auto &x : a) cin >> x;
-    
-    cout << smart(n, a) << "\n";
-}
-
-int32_t main() 
-{
-    auto begin = std::chrono::high_resolution_clock::now();
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    int t = 1;
-    // freopen("in",  "r", stdin);
-    // freopen("out", "w", stdout);
-    
- //   cin >> t;
-    for(int i = 1; i <= t; i++) 
-    {
-        //cout << "Case #" << i << ": ";
-        Solve();
-    }
-    auto end = std::chrono::high_resolution_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
-    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n"; 
-    return 0;
+std::vector<int32_t> construct_range(int32_t M, int32_t K) {
+  return {1, 1, 1};
 }
